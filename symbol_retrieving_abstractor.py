9387c089ed7f67e4b40bce221843126e06bba2b6
Awni <awnyaltabaa@gmail.com>
2023-11-11 00:55:55 -0500

    fixed import

--- symbol_retrieving_abstractor.py
+++ symbol_retrieving_abstractor.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 from abstracters import RelationalAbstracterLayer
+import numpy as np
 
 # TODO: decide on how to integrate this into code-base and name of module
 # maybe just integrate into abstracters.py?
